8935558fabc6983f48ac8d937f29f2d212192782
ImanliOmer <100156424+ImanliOmer@users.noreply.github.com>
2023-01-18 17:49:35 +0400

    Add files via upload

---
+++ Two-dimensional array/Two-dimensional array/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Two_dimensional_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //////////////////////////////////////TASK1//////////////////////////////////////////////////
+            //int[,] arr = new int[3, 3]
+            //    {
+            //      {4,6,8},
+            //      {3,5,9},
+            //      {6,8,10},
+            //    };
+            //int sumDiagonal = 0;
+            //for (int i = 0; i < arr.GetLength(0); i++)
+            //{
+            //    for (int q = 0; q < arr.GetLength(1); q++)
+            //    {
+            //        if (i == q)
+            //        {
+            //            sumDiagonal += arr[i, q];
+            //        }
+            //        Console.Write(arr[i, q]);
+            //    }
+            //    Console.WriteLine();
+            //}
+            //Console.WriteLine();
+            //Console.WriteLine(sumDiagonal);
+            /////////////////////////////////////////TASK2/////////////////////////////////////////////////////
+            //int[,] arr = new int[2, 2]
+            //{
+            //    {4,6 },
+            //    {7,6 }
+            //};
+            //int[,] numbers = new int[2, 2]
+            //{
+            //    {65, 8 },
+            //    {14,6}
+            //};
+            //int[,] sum = new int[2, 2];
+
+            //for (int i = 0; i < arr.GetLength(0); i++)
+            //{
+            //    for (int n = 0; n < arr.GetLength(1); n++)
+            //    {
+            //        sum[i, n] = arr[i, n] + numbers[i, n];
+            //        Console.Write(sum[i, n]);
+            //    }
+            //    Console.WriteLine();
+            //}
+        }
+    }
+}
